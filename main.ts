5d19e89f7336936c54f6bf84d0ed64ce1a7ca078
GregMicro <81907425+GregMicro@users.noreply.github.com>
2021-04-05 16:21:16 +0100

    Added a few functions- Amelie has an idea

--- main.ts
+++ main.ts
@@ -1,5 +1,23 @@
-let LeftDetector = 0
+/**
+ * Idea: if junction pick random direction and try again
+ */
+function IsTouchingBoth () {
+    return LeftDetector == 0 && RightDetector == 0
+}
+function IsTouchingLeft () {
+    return LeftDetector == 0 && RightDetector == 1
+}
+/**
+ * Detector = 0 -> it sees the black line
+ */
+function IsTouchingRight () {
+    return LeftDetector == 1 && RightDetector == 0
+}
+function IsNotTouching () {
+    return LeftDetector == 1 && RightDetector == 1
+}
 let RightDetector = 0
+let LeftDetector = 0
 basic.showIcon(IconNames.Happy)
 radio.setGroup(1)
 pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
@@ -8,15 +26,15 @@ kitronik_servo_lite.setDegreesPerSecond(360)
 basic.forever(function () {
     RightDetector = pins.digitalReadPin(DigitalPin.P15)
     LeftDetector = pins.digitalReadPin(DigitalPin.P16)
-    if (LeftDetector == 0 && RightDetector == 0) {
+    if (IsTouchingBoth()) {
         kitronik_servo_lite.stop()
-    } else if (LeftDetector == 1 && RightDetector == 0) {
+    } else if (IsTouchingRight()) {
         kitronik_servo_lite.turnRight(10)
         basic.pause(100)
-    } else if (LeftDetector == 0 && RightDetector == 1) {
+    } else if (IsTouchingLeft()) {
         kitronik_servo_lite.turnLeft(10)
         basic.pause(100)
-    } else if (LeftDetector == 1 && RightDetector == 1) {
+    } else if (IsNotTouching()) {
         kitronik_servo_lite.forward()
     }
 })
